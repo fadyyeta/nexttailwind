9506d0a97c9b475f85d3d8328d24583754e406df
Fady Yeta <fadyyeta@gmail.com>
2024-06-22 21:27:36 +0200

    add card and search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,41 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 export default function Home() {
+	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [term, setTerm] = useState("");
+
+	useEffect(() => {
+		fetch(
+			`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				setImages(data.hits);
+				setIsLoading(false);
+			})
+			.catch((err) => console.log(err));
+	}, [term]);
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="max-w-sm rounded overflow-hidden shadow-lg">
-				<Image
-					src="https://picsum.photos/400"
-					alt=""
-					width={400}
-					height={400}
-					className="w-full"
-				/>
-				<div className="px-6 py-4">
-					<div className="font-bold text-purple-500 text-xl mb-2">
-						Photo by John Doe
-					</div>
-					<ul>
-						<li>
-							<strong>Views: </strong>
-							4000
-						</li>
-						<li>
-							<strong>Downloads: </strong>
-							3000
-						</li>
-						<li>
-							<strong>Likes: </strong>
-							400
-						</li>
-					</ul>
+		<div className="container mx-auto">
+			<ImageSearch searchText={(text: string) => setTerm(text)} />
+
+			{!isLoading && images.length === 0 && (
+				<h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+			)}
+
+			{isLoading ? (
+				<h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+			) : (
+				<div className="grid grid-cols-3 gap-4">
+					{images.map((image) => (
+						<ImageCard key={image.id} image={image} />
+					))}
 				</div>
-				<div className="px-6 py-4">
-					<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-						#tag1
-					</span>
-					<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-						#tag2
-					</span>
-					<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-						#tag3
-					</span>
-				</div>
-			</div>
-		</main>
+			)}
+		</div>
 	);
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["picsum.photos"],
+		domains: ["picsum.photos", "pixabay.com"],
+	},
+	env: {
+		BASE_URL: process.env.BASE_URL,
+		PIXABAY_API_KEY: process.env.PIXABAY_API_KEY,
 	},
 };
 
