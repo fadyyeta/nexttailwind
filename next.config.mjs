49c5470ba30b3476d6250afcd32290fefb55d680
Fady Yeta <fadyyeta@gmail.com>
2024-06-22 21:51:36 +0200

    PIXABAY_API_KEY

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
 	},
 	env: {
 		BASE_URL: process.env.BASE_URL,
-		PIXABAY_API_KEY: process.env.PIXABAY_API_KEY,
+		PIXABAY_API_KEY: process.env.NEXT_PUBLIC_PIXABAY_API_KEY,
 	},
 };
 
