b3cce3a69a4610bf25e95773e3fbec96b055a520
Fady Yeta <fadyyeta@gmail.com>
2024-06-22 23:54:24 +0200

    tags margin

--- app/components/ImageCard.js
+++ app/components/ImageCard.js
@@ -37,7 +37,7 @@ const ImageCard = ({ image }) => {
 				{tags.map((tag, index) => (
 					<span
 						key={index}
-						className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+						className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 m-0.5">
 						#{tag}
 					</span>
 				))}
