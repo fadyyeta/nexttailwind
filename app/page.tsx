ca21f844cd1a98cf456edc1c623a85f2c8bf1a76
Fady Yeta <fadyyeta@gmail.com>
2024-06-25 10:04:53 +0200

    add gallery component

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,10 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import ImageCard from "./components/ImageCard";
-import ImageSearch from "./components/ImageSearch";
+import React from "react";
+import Gallery from "./components/Gallery"
 export default function Home() {
-	const [images, setImages] = useState<any[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [term, setTerm] = useState("");
-
-	useEffect(() => {
-		fetch(
-			`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				setImages(data.hits);
-				setIsLoading(false);
-			})
-			.catch((err) => console.log(err));
-	}, [term]);
-
+	
 	return (
 		<div className="container mx-auto">
-			<ImageSearch searchText={(text: string) => setTerm(text)} />
-
-			{!isLoading && images.length === 0 && (
-				<h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
-			)}
-
-			{isLoading ? (
-				<h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
-			) : (
-				<div className="grid grid-cols-3 gap-4">
-					{images.map((image) => (
-						<ImageCard key={image.id} image={image} />
-					))}
-				</div>
-			)}
+			<Gallery/>
 		</div>
 	);
 }
